7d6efd5c908d259db1a878284464a7ec3f7192e0
Nick Colley <nickcolley7@gmail.com>
2020-02-21 16:51:14 +0000

    Render deprecation warnings

--- config.rb
+++ config.rb
@@ -1,3 +1,13 @@
 require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
+
+helpers do
+    def markdown(text=nil, &block)
+        content = text
+        if block_given?
+            content = capture_html(&block)
+        end
+        concat Tilt['markdown'].new(context: @app) { content }.render
+    end
+end
