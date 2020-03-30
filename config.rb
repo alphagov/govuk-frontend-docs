081ae6e6db29d84275140c32163f1f2dfbda9d07
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2020-03-30 10:04:11 +0100

    Lint config.rb

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
-require 'govuk_tech_docs'
-require 'sassdocs_helpers'
+require "govuk_tech_docs"
+require "sassdocs_helpers"
 
 GovukTechDocs.configure(self)
 
 helpers do
-    include SassdocsHelpers
-    def markdown(content=nil)
-        concat Tilt['markdown'].new(context: @app) { content }.render
-    end
+  include SassdocsHelpers
+  def markdown(content = nil)
+    concat Tilt["markdown"].new(context: @app) { content }.render
+  end
 end
