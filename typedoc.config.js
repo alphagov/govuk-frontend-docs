92ab227ef40d35cdd3706846a032f6ead7af01dd
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2024-08-16 13:57:11 +0100

    Fix govuk-frontend being treated as external

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,6 +1,5 @@
 // const { join, relative } = require('path')
 
-const basePath = './node_modules/govuk-frontend/dist'
 // const workspacePath = slash(relative(paths.root, basePath))
 // const { HEROKU_APP, HEROKU_BRANCH = 'main' } = process.env
 
@@ -16,35 +15,17 @@ module.exports = {
   //   : `https://github.com/alphagov/govuk-frontend/blob/{gitRevision}/{path}#L{line}`,
 
   // Configure paths
-  basePath,
+  basePath: './node_modules/govuk-frontend/dist',
   entryPoints: ['./node_modules/govuk-frontend/dist/govuk/all.mjs'],
   tsconfig: './tsconfig.json',
   out: './jsdoc',
 
-  // Turn off strict checks for JSDoc output
-  // since `lint:types` will already log issues
-  // compilerOptions: {
-  //   allowJs: true,
-  //   checkJs: true,
-  //   esModuleInterop: true,
-  //   module: "esnext",
-  //   moduleResolution: "node10",
-  //   noEmit: true,
-  //   noImplicitThis: true,
-  //   resolveJsonModule: true,
-  //   strict: false,
-  //   strictBindCallApply: true,
-  //   strictFunctionTypes: true,
-  //   target: "esnext",
-  //   types: [],
-  //   allowSyntheticDefaultImports: true,
-  //   useDefineForClassFields: true
-  // },
-
   // Document private methods. These are behind a checkbox in the
   // settings menu of the JSDoc page.
   excludePrivate: false,
-
-  // Ignore known undocumented types (@internal, @private etc)
-  intentionallyNotExported: ['I18n', 'TranslationPluralForms']
+  
+  // Because `govuk-frontend` is in `node_modules`, TypeDoc would consider it
+  // 'external'. As it's what we're documenting, we'll instead treat anything
+  // that's not in the govuk-frontend package 'external'
+  externalPattern: ['!**/node_modules/govuk-frontend/**'],
 }
